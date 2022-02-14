759f73a5c878508d1f9dfcc9145c47aa9207e444
Piatsiushyk Ivan <dirol-88@mail.ru>
2022-02-14 13:26:40 +0300

    add dependency

---
+++ src/main/java/com/webapp/worldnews/WorldnewsApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.worldnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldnewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldnewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/worldnews/WorldnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.worldnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldnewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
